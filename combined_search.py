9dfb11248f4d5a64f1fcf700502fe886c48e52c4
stukenkemperleonie <89160634+stukenkemperleonie@users.noreply.github.com>
2026-06-10 18:21:40 +0200

    Add files via upload

--- combined_search.py
+++ combined_search.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 clip = "openai/clip-vit-base-patch32"#16 or 32, preferred 32
 vit = "google/vit-base-patch16-224-in21k"
 
-model_name = clip
+model_name = vit
 
 pic = "INDIGO_2022-03-13_Z7ii-B_0052_230516T12_10_16"
 
@@ -81,7 +81,7 @@ def get_color_features(image_path, bins=(8, 8, 8)):
 # -----------------------
 # Search
 # -----------------------
-def search(query_image, top_k=5, alpha=0.7):
+def search(query_image, top_k=5, alpha=0.5):
     """
     alpha = CLIP weight
     (1 - alpha) = color weight
@@ -98,33 +98,44 @@ def search(query_image, top_k=5, alpha=0.7):
     idx = final_score.argsort()[::-1][:top_k]
 
     return [(image_paths[i], final_score[i]) for i in idx]
-
 # -----------------------
 # Run example
 # -----------------------
 
+alphas_to_test = [0, 1, 0.5]
+all_results = {}
 
-results = search(query, top_k=10, alpha=0.5)
-results = results[1:] #removes query from list
+for a in alphas_to_test:
+    res = search(query, top_k=11, alpha=a)
+    all_results[a] = res[1:] #removes query from list
 
-print("\nTop matches:\n")
+for alpha, results in all_results.items():
+    # Determine the title based on alpha
+    if alpha == 0:
+        title_type = "COLOR ONLY\n(Alpha = 0)"
+    elif alpha == 1:
+        title_type = "CLIP ONLY\n(Alpha = 1)"
+    else:
+        title_type = f"MIXED\n(Alpha = {alpha})"
 
-for path, score in results:
-    print(f"{score:.0%}  {path}")
+    print(f"\nTop matches for {title_type}:\n")
+    for path, score in results:
+        print(f"{score:.0%}  {path}")
 
 # -----------------------
 # Visualize
 # -----------------------
-fig, axes = plt.subplots(1, len(results) + 1, figsize=(18, 5))
+    fig, axes = plt.subplots(1, len(results) + 1, figsize=(18, 5))
 
-axes[0].imshow(Image.open(query))
-axes[0].set_title("QUERY")
-axes[0].axis("off")
+    axes[0].imshow(Image.open(query))
+    axes[0].set_title(f"QUERY\n({title_type})")
+    axes[0].axis("off")
 
-for ax, (path, score) in zip(axes[1:], results):
-    ax.imshow(Image.open(path))
-    ax.set_title(f"{score:.0%}")
-    ax.axis("off")
+    for ax, (path, score) in zip(axes[1:], results):
+        ax.imshow(Image.open(path))
+        ax.set_title(f"{score:.0%}")
+        ax.axis("off")
 
-plt.tight_layout()
+    plt.tight_layout()
+
 plt.show()
